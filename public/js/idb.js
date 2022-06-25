aeb7db25106c6c7e1d5f64020800b6b32fdd14a5
Lady Trell <antrellkent@gmail.com>
2022-06-25 10:34:29 -0400

    upLoadTransaction fetch api/transaction/bulk

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,7 +9,7 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_transaction', { autoIncrement: true });
-  };
+};
 
 // upon a successful 
 request.onsuccess = function(event) {
@@ -21,12 +21,12 @@ request.onsuccess = function(event) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
       uploadTransaction();
     }
-  };
+};
   
-  request.onerror = function(event) {
+request.onerror = function(event) {
     // log error here
     console.log(event.target.errorCode);
-  };
+};
 
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
@@ -38,9 +38,9 @@ function saveRecord(record) {
   
     // add record to your store with add method
     transactionObjectStore.add(record);
-  }
+}
   
-  function uploadTransaction() {
+function uploadTransaction() {
     // open a transaction on your db
     const transaction = db.transaction(['new_transaction'], 'readwrite');
   
@@ -54,7 +54,7 @@ function saveRecord(record) {
     getAll.onsuccess = function() {
         // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
-        fetch('/api/transactions', {
+        fetch('api/transaction/bulk', {
             method: 'POST',
             body: JSON.stringify(getAll.result),
             headers: {
@@ -84,7 +84,7 @@ function saveRecord(record) {
             });
         }
     };
-  }
+}
 
 // listen for app coming back online
 window.addEventListener('online', uploadTransaction);
